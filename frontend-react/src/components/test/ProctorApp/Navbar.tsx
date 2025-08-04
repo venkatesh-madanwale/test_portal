c4bcdd3ed2e8ccc6f351140be26d2d33c97a633f
venkatesh-madanwale <venkateshjmad@gmail.com>
2025-08-04 10:32:06 +0530

    Integration of Proctal and main

--- frontend-react/src/components/test/ProctorApp/Navbar.tsx
+++ frontend-react/src/components/test/ProctorApp/Navbar.tsx
@@ -5,9 +5,9 @@ import { type RootState } from "../../../redux/store";
 const Navbar = () => {
   const capturedImage = useSelector((state: RootState) => state.proctor.capturedImage);
   return (
-    <nav className="navbar">
-      <h2 className="navbar-title" style={{"color":"green"}}>mirafra</h2>
-      <div className="navbar-right">
+    <nav className="navbarr">
+      <h2 className="navbarr-title" style={{"color":"green"}}>mirafra</h2>
+      <div className="navbarr-right">
         {capturedImage && (
           <img
             src={capturedImage}
